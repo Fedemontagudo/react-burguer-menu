1ee5a279537939355c43007931fdd01e63a9dc10
Fede Montagud <fedemontagudo@gmail.com>
2021-05-25 16:44:17 +0200

    styles of the menu in jsx

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,14 @@
+import Slide from "./Slide";
 
 
 function App() {
+  // aquí sota del return, importarem un altre component que serà l'slide lateral. Aquest slide lateral tindra els elements del menu burger. Tindra
+  // tb una classe oculto i de bones a primeres no es mostrara. Es necessitará un preventdefault. El clic al icono es recollirá i treura la classe
+  // oculto, de manera que apareixera el menú. Després d'axió quedará animar la transició d'aparició del menú,
+  // finalment s'animará l'icono perque giri
   return (
     <>
-
+      <Slide></Slide>
     </>
   );
 }
--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Slide() {
   return (
-    <div>
-
+    <div className="slideMenu">
+      <ul className="slideList">
+        <li>Home</li>
+        <li>Who are we?</li>
+        <li>Bussiness Inquiries</li>
+        <li>Work with us</li>
+        <li>Contact</li>
+      </ul>
     </div>
   )
 }
